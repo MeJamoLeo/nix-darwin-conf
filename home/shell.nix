670a7d8de7b517a1686514bf3905665616881c4f
treo <sample@sample.com>
2025-04-27 22:52:59 +0900

    Add initial Nix configuration for macOS using flake
    - Created .gitignore to exclude result directory.
    - Added flake.nix and flake.lock for Nix flake management.
    - Introduced Justfile for command automation.
    - Established README.md with usage instructions and configuration structure.
    - Configured home-manager and system settings in various Nix modules.
    - Implemented scripts for proxy setup and user management.
    - Included essential packages and applications for a comprehensive macOS setup.

---
+++ home/shell.nix
@@ -0,0 +1,18 @@
+{...}: {
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    initExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+      bindkey -e  # emacsモードを有効化（Ctrl+N, Ctrl+Pが使えるようになる）
+      eval "$(direnv hook zsh)" # direnvを有効化, direnvは環境変数を管理するツール
+    '';
+  };
+
+  home.shellAliases = {
+    k = "kubectl";
+
+    urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
+    urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+  };
+}
