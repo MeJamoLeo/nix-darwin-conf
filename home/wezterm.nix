ca2a35eeba77c403bda65a41e0a3b901a68f4e41
MeJamoLeo <55238651+MeJamoLeo@users.noreply.github.com>
2025-12-20 21:11:23 -0600

    feat: enhance wezterm configuration with new visual settings

--- home/wezterm.nix
+++ home/wezterm.nix
@@ -6,6 +6,19 @@
     extraConfig = ''
       return {
         font_size = 20.0,
+        color_scheme = 'Kasugano (terminal.sexy)',
+        window_background_opacity = 0.8,
+        macos_window_background_blur = 20,
+        window_decorations = "RESIZE",
+        show_tabs_in_tab_bar = true,
+        hide_tab_bar_if_only_one_tab = true,
+        window_frame = {
+          inactive_titlebar_bg = "none",
+          active_titlebar_bg = "none",
+        },
+        window_background_gradient = {
+          colors = { "#000000" },
+        }
       }
     '';
   };
